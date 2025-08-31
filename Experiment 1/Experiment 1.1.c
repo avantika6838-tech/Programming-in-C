6a7894f5670d8c27607ff4ff9a025086baf24c82
avantika6838-tech <avantika.6838@gmail.com>
2025-08-31 15:49:41 +0530

    Update Experiment 1.1.c

--- Experiment 1/Experiment 1.1.c
+++ Experiment 1/Experiment 1.1.c
@@ -1,1 +1,12 @@
+/*Experiment 1: Installation, Environment Setup and starting with C language
+1. Write a C program to print “ Hello World” */
+
+#include<stdio.h>
+int main()
+{
+	printf("Name - Avantika\nSap ID - 590028347\nCourse - Bsc CS\nBatch - 1st");
+	printf("\n-----------------------------------\n");
+	printf("Hello world");
+	return 0;
+}
 
