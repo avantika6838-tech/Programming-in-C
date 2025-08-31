998e0d6e202d03d1044d87713ca74e9a55cec03a
avantika6838-tech <avantika.6838@gmail.com>
2025-08-31 15:50:45 +0530

    Update Experiment 1.2.c

--- Experiment 1/Experiment 1.2.c
+++ Experiment 1/Experiment 1.2.c
@@ -1,1 +1,11 @@
+/*Experiment 1: Installation, Environment Setup and starting with C language
+2. Write a C Program to print the address in multiple lines (new line)*/
+#include<stdio.h>
+int main()
+{
+	printf("Name - Avantika\nSap ID - 590028347\nCourse - Bsc CS\nBatch - 1st");
+	printf("\n-----------------------------------\n");
+               printf("House no.344,ward no.4\n Rajput Lane\n Gharaunda ,Karnal\n Haryana -        132114 ");
+	return 0;
+}
 
