9932611c3373217cda17a5373dc966ee025fb8fe
avantika6838-tech <avantika.6838@gmail.com>
2025-10-24 15:11:40 +0530

    Experiment 6.8.c
    This program prints a specific pattern along with user details.

---
+++ Experiment 6 /Experiment 6.8.c
@@ -0,0 +1,48 @@
+/*4.3.8*/
+#include<stdio.h>
+int main()
+{
+    printf("Name - Avantika\nSap ID - 590028347\nCourse - Bsc CS\nBatch - 1st");
+    printf("\n-----------------------------------\n");
+
+printf("The pattern is:\n");
+int i, j, k, l;
+for(i=1; i<=5; i++)
+{
+for(k=5; k>=i; k--)
+{
+printf(" ");
+}
+for(j=1; j<=i; j++)
+{
+printf("%d ", j);
+}
+int m=i-1;
+for(l=1; l<=i; l++)
+{
+if(m==0)
+printf(" ");
+else
+printf("%d ", m);
+m--;
+}
+printf("\n");
+}
+for(i=4; i>=1; i--)
+{
+for(k=i; k<=5; k++)
+{
+printf(" ");
+}
+for(j=1; j<=i; j++)
+{
+printf("%d ", j);
+}
+for(l=i-1; l>=1; l--)
+{
+printf("%d ", l);
+}
+printf("\n");
+}
+return 0;
+}
